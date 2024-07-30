92896f2181f67b1b0bb6998852e313e3db702289
Mason Brown <masubrow@ucsc.edu>
2024-07-30 13:30:12 -0700

    Setting up functions and more libraries

--- initial_commit.py
+++ initial_commit.py
@@ -1,3 +1,17 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+#Residual block is defined
+def residual_block(parameters):
+    return
+
+#ResNet18 model is built using the residual blocks
+def build_resnet18(input_shape, num_classes):
+    return
+
+#Two different datasets are created for training and validation
+train_dataset = 0
+validation_dataset = 0
