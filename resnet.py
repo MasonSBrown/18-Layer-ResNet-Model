c851dd014734e40b38b5f93a46c3df7c518d97f3
Mason Brown <masubrow@ucsc.edu>
2024-07-31 00:08:33 -0700

    Switching from tensorflow to PyTorch

--- resnet.py
+++ resnet.py
@@ -1,40 +1,27 @@
-import tensorflow as tf
-from tensorflow.keras import layers, models
-import numpy as np
-import pandas as pd
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models
 import zipfile
 import os
 import matplotlib.pyplot as plt
 
 #Two different datasets are created for training and validation
-val_dir = os.path.join('archive', "val")
+val_dir = os.path.join('archive', "test")
 train_dir = os.path.join('archive', "train")
 
-#Parameters
-batch_size = 32
-img_height = 224
-img_width = 224
+train_transforms = transforms.Compose([
+    transforms.Resize((224, 224)), #Makes all images 224 by 224 pixels
+    transforms.RandomHorizontalFlip(), #randomly flips and helps generalize the model better
+    transforms.ToTensor(), #convers image from PIL format to Pytorch tensor (necessary for input)
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    #Normalizes images with "ImageNet" statistics, which ResNet was trained on.
+])
 
-#Loads images from specified directory and creates a 'tf.data.Dataset' object
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    train_dir,
-    seed=123, #random seed for shuffling and transformations
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+val_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    val_dir,
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-
-print("Tensorflow is intalled. Version:", tf.__version__)
-
-#Residual block is defined
-def residual_block(parameters):
-    return
-
-#ResNet18 model is built using the residual blocks
-def build_resnet18(input_shape, num_classes):
-    return
 
